2fa19c4a4e0ffbcbe8dd99c63ca0c2a5a71fbf84
NomsuBen <olaniranbenjamin@gmail.com>
2024-08-09 11:52:17 -0400

    API for Search Service Created

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -21,6 +22,13 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
+const giftRoutes = require('./routes/giftRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const pinoHttp = require('pino-http');
+const logger = require('./logger');
+
+app.use(pinoHttp({ logger }));
+
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
 
@@ -34,6 +42,9 @@ const logger = require('./logger');
 app.use(pinoHttp({ logger }));
 
 // Use Routes
+app.use('/api/gifts', giftRoutes);
+app.use('/api/search', searchRoutes)
+
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use('/api/gifts', giftRoutes);
 
