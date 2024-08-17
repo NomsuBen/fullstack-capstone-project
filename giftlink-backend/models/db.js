8e321ce0daf5017779a84df9bca76319abb8126e
NomsuBen <olaniranbenjamin@gmail.com>
2024-08-17 03:28:07 -0400

    API for Gift Service updated

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,4 +1,3 @@
-// db.js
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
@@ -9,14 +8,19 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+    if (dbInstance) {
+        return dbInstance;
+    }
 
     const client = new MongoClient(url);
 
+    // Task 1: Connect to MongoDB
     await client.connect();
+
+    // Task 2: Connect to database giftDB and store in variable dbInstance
     dbInstance = client.db(dbName);
+
+    // Task 3: Return database instance
     return dbInstance;
 }
 
