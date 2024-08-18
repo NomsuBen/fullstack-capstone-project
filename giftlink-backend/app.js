432922bf284ac80508a4be9f4a9677bea582ceee
NomsuBen <olaniranbenjamin@gmail.com>
2024-08-17 20:50:06 -0400

    Backend updated

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -26,16 +26,6 @@ const pinoHttp = require('pino-http');
 const logger = require('./logger');
 app.use(pinoHttp({ logger }));
 
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-const giftRoutes = require('./routes/giftRoutes');
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-const searchRoutes = require('./routes/searchRoutes');
-
-const pinoHttp = require('pino-http');
-const logger = require('./logger');
-
-app.use(pinoHttp({ logger }));
 
 // Use Routes
 app.use('/api/auth', authRoutes);
